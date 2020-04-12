ce1871079586b14cd4d61d2986c6677e1fc70694
Luciano Renzi <luciano@lucianorenzi.com>
2020-04-12 15:37:15 -0300

    expand delete_execution, get_suite_execution

--- projects/golem_api/tests/report/delete_execution.py
+++ projects/golem_api/tests/report/delete_execution.py
@@ -7,12 +7,22 @@ from projects.golem_api.pages import report
 
 def setup(data):
     project.using_project('general')
-    project.create_random_test(data.project)
-    response = suite.run_suite(data.project, '.')
-    actions.store('timestamp', response.json())
 
 
 def test(data):
-    response = report.delete_execution(data.project, 'all', data.timestamp)
+    # execution does not exist
+    suite_name = project.create_random_suite(data.project)
+    timestamp = actions.random_str()
+    response = report.delete_execution(data.project, suite_name, timestamp)
+    assert response.status_code == 200
+    assert response.json() == ['Execution for {} {} {} does not exist'.format(data.project, suite_name, timestamp)]
+
+    # delete execution
+    response = suite.run_suite(data.project, suite_name)
+    timestamp = response.json()
+    report.wait_for_execution_to_finish(data.project, suite_name, timestamp)
+
+    response = report.delete_execution(data.project, suite_name, timestamp)
+
     assert response.status_code == 200
     assert response.json() == []
